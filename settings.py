c6cd05a0bed3180c7e766f41372567571c8f5e3e
VitorinoAssuncao <vitorino.muller@gmail.com>
2020-09-25 17:46:48 -0600

    alteração procfile

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ MYSQL_DATABASE_DB = "basket"
 MYSQL_DATABASE_HOST = "localhost"
 MYSQL_DATABASE_PORT = "3306"
 SERVER_HOST_NAME = "localhost"
-SQLALCHEMY_DATABASE_URI = os.path.join('mysql+pymysql://dev:dev@localhost/basket')
+SQLALCHEMY_DATABASE_URI = os.path.join('postgres://krbasfbcuwugru:ecd961db27e0cfe5411b8fb0ee92ac03a68dc232d8ebdcd98879c4661fe82787@ec2-54-165-164-38.compute-1.amazonaws.com:5432/df5rtq003qd3el')
 SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS',True)
 
 HOST = 'localhost'
