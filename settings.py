11387bfecc23633a2f48aae3ef603cd3e2ec4a2c
VitorinoAssuncao <vitorino.muller@gmail.com>
2020-09-25 21:12:01 -0600

    ajuste no settings

--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 import os
-FLASK_APP = 'main'
+FLASK_APP = 'run'
 SERVER_HOST_NAME = "localhost"
 SQLALCHEMY_DATABASE_URI = os.path.join('postgres://krbasfbcuwugru:ecd961db27e0cfe5411b8fb0ee92ac03a68dc232d8ebdcd98879c4661fe82787@ec2-54-165-164-38.compute-1.amazonaws.com:5432/df5rtq003qd3el')
 SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS',True)
 
-HOST = '127.0.0.1'
-PORT = int(os.environ.get("PORT", default=5000))
+HOST = '0.0.0.0'
+PORT = str(os.environ.get("PORT", 5000))
 DEBUG = True
 SECRET_KEY = 'basket'
